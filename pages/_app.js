44f0f8f290570e70513226c34a06d61aa9411317
mfh2010-sudo <mfh2010@gmail.com>
2026-07-01 09:28:11 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/globals.css';
-import '../styles/admin.css';
+import '../styles2/globals.css';
+import '../styles2/admin.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
